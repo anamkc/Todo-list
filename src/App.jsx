a0f0a07341efb30abf2d224cae464619c5047e0f
anamkc <anamkhatri10@gmail.com>
2023-11-11 23:09:09 +0545

    file name changed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const[data , setData] = useState();
 
   return (
     <div className="h-screen flex justify-center items-center   bg-slate-600 ">
-    <div className="h-2/3 h-fit w-1/2 bg-orange-400 rounded-2xl  ">
+    <div className=" h-fit w-1/2 bg-orange-400 rounded-2xl  ">
      <h1 className="text-2xl text-center m-6 ">Todo List</h1>
     <Input/>
      </div>
